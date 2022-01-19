7c92051e1d314b54a836d7478f07886bdcef0aad
Alexey Davidenko <alexeydavidenko@icloud.com>
2022-01-19 14:56:08 +0300

    deleted comments in viewController

--- ForkWork/ViewController.swift
+++ ForkWork/ViewController.swift
@@ -1,17 +1,9 @@
-//
-//  ViewController.swift
-//  ForkWork
-//
-//  Created by Alexey Davidenko on 19.01.2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
 
